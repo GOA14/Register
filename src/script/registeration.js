31517d056994b8f2902da88e9bbc2df612760bc8
Grace Adeloye <graceadeloye2017@yahoo.com>
2022-12-12 03:51:05 +0700

    register

---
+++ src/script/registeration.js
@@ -0,0 +1,11 @@
+const userBtn = document.querySelector(".userBtn");
+const adminBtn = document.querySelector(".adminBtn");
+const userForm = document.querySelector(".user-form");
+const adminForm = document.querySelector(".admin-form");
+
+userBtn.addEventListener("click", showUserForm);
+
+const showUserForm = () => {
+    // userForm.style.display = "block";
+    console.log("click")
+}
