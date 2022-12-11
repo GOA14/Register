a75ea2a4678aa666df275f8eda0668960c1a0b8d
Nafiu Taiwo <69268373+taiwonaf@users.noreply.github.com>
2022-12-11 23:13:29 +0100

    Taiwo

--- src/script/registeration.js
+++ src/script/registeration.js
@@ -4,8 +4,24 @@ const userForm = document.querySelector(".user-form");
 const adminForm = document.querySelector(".admin-form");
 
 userBtn.addEventListener("click", showUserForm);
+adminBtn.addEventListener("click", showAdminForm);
 
-const showUserForm = () => {
-    // userForm.style.display = "block";
-    console.log("click")
+document.onload = showUserForm();
+
+function showUserForm() {
+    userForm.style.display = "grid";
+    adminForm.style.display = "none";
+    userBtn.style.background = "#CFCCD2";
+    userBtn.style.color = "black";
+    adminBtn.style.background = "#663398";
+    adminBtn.style.color = "white";
+}
+
+function showAdminForm() {
+    adminForm.style.display = "grid";
+    userForm.style.display = "none";
+    adminBtn.style.background = "#CFCCD2";
+    adminBtn.style.color = "black";
+    userBtn.style.background = "#663398";
+    userBtn.style.color = "white";
 }
